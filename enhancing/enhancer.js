3de64fa7c79022c13caa7ef3fc06de91bd1963d7
Giovani Garfias <46204471+survani@users.noreply.github.com>
2020-04-29 19:56:18 -0700

    MVP Completed

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,54 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  // if enhancement is less than 20 you will get a +1
+  if (item.enhancement < 20) {
+    const successItem = {
+      name: item.name,
+      durability: item.durability,
+      enhancement: item.enhancement + 1,
+    };
+    return successItem;
+    // if enhancement is === 20 you will get no change on the enhancement
+  } else if (item.enhancement === 20) {
+    const successItem = {
+      name: item.name,
+      durability: item.durability,
+      enhancement: item.enhancement,
+    };
+    return successItem;
+    // if enhancement reaches a NUMBER > 20 display error.
+  } else if (item.enhancement > 20 || item.durability > 100) {
+    const error = "Please don't try to add a bigger number than 20.";
+    return error;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    const failItem = {
+      name: item.name,
+      durability: item.durability - 5,
+      enhancement: item.enhancement,
+    };
+    return failItem;
+  } else if (item.enhancement >= 15 && item.enhancement > 16) {
+    const failItem = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement - 1,
+    };
+    return failItem;
+  }
 }
-
+//when an item is repaired the only thing that changes is the durabiility.
 function repair(item) {
-  return { ...item };
+  const repairedItem = {
+    name: item.name,
+    durability: 100,
+    enhancement: item.enhancement,
+  };
+  return repairedItem;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,138 @@
-const enhancer = require('./enhancer.js');
+const { repair, succeed, fail } = require("./enhancer.js");
 // test away!
+
+//Everything related to repair()...
+//passes = completed && Fail = Completed
+// -- TODO => If durability is set to be greater than 100 it should not pass.
+describe("enhancer.js", () => {
+  describe("repair()", () => {
+    it("should make durability from Number to 100...", () => {
+      const items = {
+        name: "Giovani",
+        durability: 15,
+        enhancement: 20,
+      };
+
+      const expects = {
+        name: "Giovani",
+        durability: 100,
+        enhancement: 20,
+      };
+
+      const itemRepaired = repair(items);
+
+      expect(itemRepaired).toEqual(expects);
+    });
+  });
+});
+
+//Everything related to success()...
+// You are able to check all conditions for succeed() function.
+//passes = completed && Fail = Completed
+describe("enhancer.js", () => {
+  describe("success()", () => {
+    it("should make enhancement current + 1", () => {
+      const items = {
+        name: "Giovani",
+        durability: 12,
+        enhancement: 18,
+      };
+
+      const expects = {
+        name: "Giovani",
+        durability: 12,
+        enhancement: 19,
+      };
+
+      const itemEnhanced = succeed(items);
+
+      expect(itemEnhanced).toEqual(expects);
+    });
+  });
+});
+
+//passes = completed && Fail = Completed
+describe("enhancer.js", () => {
+  describe("success()", () => {
+    it("should make enhancement state = unchanged", () => {
+      const items = {
+        name: "Giovani",
+        durability: 12,
+        enhancement: 20,
+      };
+
+      const expects = {
+        name: "Giovani",
+        durability: 12,
+        enhancement: 20,
+      };
+
+      const itemEnhanced = succeed(items);
+
+      expect(itemEnhanced).toEqual(expects);
+    });
+  });
+});
+
+//Everything related to fail()...
+//passes = completed && Fail = Completed
+describe("enhancer.js", () => {
+  describe("fail()", () => {
+    it("if enhancement of (item) < 15 = durability - 5", () => {
+      const items = {
+        name: "Giovani",
+        durability: 100,
+        enhancement: 13,
+      };
+      const expects = {
+        name: "Giovani",
+        durability: 95,
+        enhancement: 13,
+      };
+      const itemFail = fail(items);
+
+      expect(itemFail).toEqual(expects);
+    });
+  });
+});
+
+//passes = completed && Fail = Completed
+describe("enhancer.js", () => {
+  describe("fail()", () => {
+    it("if enhancement of (item) >= 15 = durability - 10", () => {
+      const items = {
+        name: "Giovani",
+        durability: 100,
+        enhancement: 17,
+      };
+      const expects = {
+        name: "Giovani",
+        durability: 90,
+        enhancement: 16,
+      };
+      const itemFail = fail(items);
+
+      expect(itemFail).toEqual(expects);
+    });
+  });
+});
+
+describe("enhancer.js", () => {
+  describe("fail()", () => {
+    it("if enhancement of (item) > 16 = enhancement -1", () => {
+      const item = {
+        name: "Giovani",
+        durability: 50,
+        enhancement: 18,
+      };
+      const expects = {
+        name: "Giovani",
+        durability: 40,
+        enhancement: 17,
+      };
+      const itemFail = fail(item);
+
+      expect(itemFail).toEqual(expects);
+    });
+  });
+});
